d6768302893b3eaa2ec84505ac9f08a7e8e847e3
Jascha Ehrenreich <jascha@jaeh.at>
2014-10-23 04:23:42 +0200

    magic-log first commit

---
+++ log.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var color = require('bash-color')
+  , verbose = true
+;
+
+module.exports = function(str, type) {
+  if (str) {
+    if ( type === 'error') {
+      str = color.red(str);
+    } else if ( type === 'success' ) {
+      str = color.green(str);
+    } else if ( type === 'warning' ) {
+      str = color.yellow(str);
+    }
+
+    console.log(str); }
+  }
+}
