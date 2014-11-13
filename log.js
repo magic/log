a4e73e0b579433502dda44c97dafc790e725151e
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-13 03:28:30 +0100

    added log functions that actually are console.log calls

--- log.js
+++ log.js
@@ -1,19 +1,21 @@
 'use strict';
-
 var color = require('bash-color')
   , verbose = true
+  , log
 ;
-
-module.exports = function(str, type) {
-  if (str) {
-    if ( type === 'error') {
-      str = color.red(str);
-    } else if ( type === 'success' ) {
-      str = color.green(str);
-    } else if ( type === 'warning' ) {
-      str = color.yellow(str);
-    }
-
-    console.log(str);
-  }
+log = function() {
+  Function.prototype.apply.apply(console.log, [console, arguments]);
 }
+log.warn = function () {
+  arguments[0] = color.yellow(arguments[0]);
+  Function.prototype.apply.apply(console.log, [console, arguments]);
+}
+log.error = function() {
+  arguments[0] = color.red(arguments[0]);
+  Function.prototype.apply.apply(console.log, [console, arguments]);
+}
+log.success = function() {
+  arguments[0] = color.green(arguments[0]);
+  Function.prototype.apply.apply(console.log, [console, arguments]);
+}
+module.exports = log;
