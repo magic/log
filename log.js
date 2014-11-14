167b84537282196cfaa58b0efe8a278ebab2b683
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-14 22:44:52 +0100

    reversed usage of this scope

--- log.js
+++ log.js
@@ -4,33 +4,30 @@ var color   = require('bash-color')
 ;
 
 log = function() {
-  
-  this.verbose = true;
+  if ( log.verbose ) {
+    Function.prototype.apply.apply(console.log, [console, arguments]);
+  }
+}
 
-  this.warn = function () {
-    arguments[0] = color.yellow(arguments[0]);
+log.verbose = true;
+
+log.warn = function () {
+  arguments[0] = color.yellow(arguments[0]);
+  Function.prototype.apply.apply(console.log, [console, arguments]);
+}
+log.error = function() {
+  arguments[0] = color.red(arguments[0]);
+  Function.prototype.apply.apply(console.log, [console, arguments]);
+}
+log.success = function() {
+  if ( log.verbose ) {
+    arguments[0] = color.green(arguments[0]);
     Function.prototype.apply.apply(console.log, [console, arguments]);
   }
-  this.error = function() {
-    arguments[0] = color.red(arguments[0]);
+}
+log.info = function() {
+  if ( log.verbose ) {
     Function.prototype.apply.apply(console.log, [console, arguments]);
-  }
-  this.success = function() {
-    if ( log.verbose ) {
-      arguments[0] = color.green(arguments[0]);
-      Function.prototype.apply.apply(console.log, [console, arguments]);
-    }
-  }
-  this.info = function() {
-    if ( log.verbose ) {
-      Function.prototype.apply.apply(console.log, [console, arguments]);
-    }
-  }
-  //this function will actually be called
-  return function() {
-    if ( log.verbose ) {
-      Function.prototype.apply.apply(console.log, [console, arguments]);
-    }
   }
 }
 
