fb6ee1a735988cdd32529e3ff36505c5a5ce97ca
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-30 03:01:12 +0200

    split functions from loop

--- src/lib/stringify.js
+++ src/lib/stringify.js
@@ -1,28 +1,30 @@
 const is = require('@magic/types')
 
+const stringifyByType = a =>  {
+  if (is.null(a) || is.undefined(a)) {
+    return '' + a
+  } else if (is.string(a) || is.number(a)) {
+    return a
+  } else if (is.array(a)) {
+    return stringify(a)
+  } else if (is.object(a)) {
+    if (is.function(a) || is.date(a)) {
+      return a.toString()
+    }
+
+    return JSON.stringify(a)
+  }
+
+  return '' + a
+}
+
+const byEmptyString = t => t !== ''
+
 const stringify = (...str) =>
-  str.length === 1
+  is.len.eq(str, 1)
     ? str[0]
-    : str
-        .map(a => {
-          if (is.null(a) || is.undefined(a)) {
-            return '' + a
-          }
-          if (is.string(a) || is.number(a)) {
-            return a
-          } else if (is.array(a)) {
-            return stringify(a)
-          } else if (is.object(a)) {
-            if (is.function(a) || is.date(a)) {
-              return a.toString()
-            }
-
-            return JSON.stringify(a)
-          }
-
-          return '' + a
-        })
-        .filter(t => t !== '')
-        .join(' ')
+    : str.map(stringifyByType)
+      .filter(byEmptyString)
+      .join(' ')
 
 module.exports = stringify
