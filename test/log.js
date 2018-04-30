495ae9042971f16b3694fb525625d0e4f67f0eca
Jascha Ehrenreich <jascha@jaeh.at>
2018-05-01 01:09:54 +0200

    remove mockconsole, update logging tests

--- test/log.js
+++ test/log.js
@@ -1,61 +1,18 @@
-const { is } = require('@magic/test')
+const { is, mock } = require('@magic/test')
 
 const log = require('../src')
 const colorize = require('../src/lib/colorize')
 
-const cache = {
-  log: [],
-  error: [],
-  warn: [],
-  success: [],
-  annotate: [],
-}
-
-const consoleMock = {
-  log: (...args) => {
-    cache.log.push(args)
-    return args
-  },
-  error: (...args) => {
-    cache.error.push(args)
-    return args
-  },
-  warn: (...args) => {
-    cache.warn.push(args)
-    return args
-  },
-  success: (...args) => {
-    cache.success.push(args)
-    return args
-  },
-  annotate: (...args) => {
-    cache.annotate.push(args)
-    return args
-  },
-}
-
-const isProd = ['-p', '--prod', '--production'].some(e => process.argv.indexOf(e) > -1)
-
-const dumpCache = () => {
-  Object.entries(cache).forEach(([k, v]) => {
-    if (k === 'error') {
-      while (v.length) {
-        console[k](v.shift())
-      }
-    }
-  })
-}
-
 const beforeAll = () => {
   global.oldConsole = console
-  global.console = consoleMock
+  global.console = mock.log
 
   return () => {
     global.console = global.oldConsole
-
-    dumpCache()
   }
 }
+
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   beforeAll,
@@ -66,25 +23,9 @@ module.exports = {
       info: 'log.log is the same function as log',
     },
     { fn: () => log.info('test'), expect: isProd ? false : is.deep.equal(['test']) },
-    { fn: () => log.error('test'), expect: is.deep.equal([colorize('test')]) },
-    {
-      fn: () => log.error('test', 'test2'),
-      expect: is.deep.equal([colorize('red', 'test'), 'test2']),
-      info: 'Only first argument is red',
-    },
-    {
-      fn: () => log.warn('test', 'test2'),
-      expect: is.deep.equal([colorize('yellow', 'test'), 'test2']),
-      info: 'Only first argument is yellow',
-    },
-    {
-      fn: () => log.success('test', 'test2'),
-      expect: isProd ? false : is.deep.equal([colorize('green', 'test'), 'test2']),
-      info: 'Only first argument is green',
-    },
     {
       fn: () => log.annotate('test', 'test2'),
-      expect: isProd ? false : is.deep.equal([colorize('grey', 'test test2')]),
+      expect: isProd ? false : undefined,
       info: 'All arguments are grey',
     },
   ],
--- test/mockConsole.js
+++
@@ -1,3 +0,0 @@
-const console = {
-  log: msg => {},
-}
