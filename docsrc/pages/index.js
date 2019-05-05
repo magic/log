abb020fdd1def6b3c13a2ab2f1d242dee5937120
Jascha <jascha@jaeh.at>
2019-05-05 02:29:58 +0200

    Pre.View to Pre

--- docsrc/pages/index.js
+++ docsrc/pages/index.js
@@ -11,7 +11,7 @@ module.exports = () => [
   p('be in a nodejs project.'),
 
   h3({ id: 'install' }, 'install'),
-  Pre.View('npm i --save-dev @magic/log'),
+  Pre('npm i --save-dev @magic/log'),
 
   h2({ id: 'usage' }, 'usage:'),
 
@@ -20,29 +20,29 @@ module.exports = () => [
   div([
     h4({ id: 'log-levels-all' }, 'all'),
     p('set logLevel to all'),
-    Pre.View(`
+    Pre(`
 log.setLevel('all')
 log.setLevel(0)`),
 
     h4({ id: 'log-levels-warnings' }, 'warnings'),
     p('only output warnings and errors'),
-    Pre.View(`
+    Pre(`
 log.setLevel('warn')
 log.setLevel(1)`),
 
     h4({ id: 'log-levels-errors' }, 'errors'),
     p('only log on error'),
-    Pre.View(`
+    Pre(`
 log.setLevel('error')
 log.setLevel(2)`),
 
     h4({ id: 'log-levels-resetlevel' }, 'log.resetLevel'),
     p('if production, set logLevel to "warn", if development, "all"'),
-    Pre.View('log.resetLevel()'),
+    Pre('log.resetLevel()'),
 
     h4({ id: 'log-levels-getlevel' }, 'log.getLevel()'),
     p('get current logLevel'),
-    Pre.View('log.getLevel()'),
+    Pre('log.getLevel()'),
   ]),
 
   h3({ id: 'log-functions' }, 'log functions'),
@@ -50,23 +50,23 @@ log.setLevel(2)`),
   div([
     h4({ id: 'log-functions-info' }, 'log.info'),
     p("only outputs if logLevel === 'all'"),
-    Pre.View(`log.info('Some interesting yet useless information')`),
+    Pre(`log.info('Some interesting yet useless information')`),
 
     h4({ id: 'log-functions-success' }, 'log.success'),
     p("only outputs if logLevel === 'all'."),
     p('output of first argument is green.'),
-    Pre.View(`log.success('yay', 'only the first', 'argument was green')`),
+    Pre(`log.success('yay', 'only the first', 'argument was green')`),
 
     h4({ id: 'log-functions-error' }, 'log.error'),
     p('always outputs. first argument will be red'),
-    Pre.View(`log.error('ERROR:', 'error messsage')`),
+    Pre(`log.error('ERROR:', 'error messsage')`),
 
     h4({ id: 'log-functions-warn' }, 'warn'),
     p("outputs if logLevel === 'warn' || 'all'"),
-    Pre.View("log.warn('WARN:', 'warn message')"),
+    Pre("log.warn('WARN:', 'warn message')"),
 
     h4({ id: 'log-functions-annotate' }, 'annotate'),
-    Pre.View("log.annotate('this message is subtle and greyed out')"),
+    Pre("log.annotate('this message is subtle and greyed out')"),
   ]),
 ]
 
